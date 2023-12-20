7b2d892c343f2f88b6c66f5441a9150c66e90da9
Var <146992931+IfVar@users.noreply.github.com>
2023-12-20 09:31:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
-x
+/* NPMs */
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const requestIP = require('request-ip');
+const session = require('express-session');
+const https = require('https');
+const fs = require('fs');
+
+var randomstring = require("randomstring");
+
+const port = 3000;
+
+// Paths
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Use */
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'locked')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+  secret: "xapplex",
+  resave: false,
+  saveUninitialized: true
+}));
+
+/* Post */
+// Something Goes Here
+
+/* Get */
+// Redirects Users From "/" to "/home"
+app.get('/', function(req, res) {
+    res.redirect("/home");
+});
+
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', '_home/index.html'));
+});
+
+//Invalid Path Redirects To /home
+app.get('*', function(req, res) {
+    res.redirect("/home");
+})
+
+/* Port */
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
